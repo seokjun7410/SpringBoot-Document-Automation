311c84ae4370f5b99aa84bae908205d219797181
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2024-01-01 18:38:39 +0900

    init project

---
+++ src/main/java/com/document/automation/SpringBootDocumentAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.document.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDocumentAutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDocumentAutomationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/document/automation/SpringBootDocumentAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.document.automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDocumentAutomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
